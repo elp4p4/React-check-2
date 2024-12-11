c679112abaf8e7b79aba368a938d4caf9803c4d3
elp4p4 <brazfu145@gmail.com>
2024-12-11 21:02:16 +0100

    default props

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl, teamIcon, nationalityIcon}) => {
+const Player = ({ name = "Alessandro Costacurta", 
+  team = "AC Milan", 
+  nationality = "Italian", 
+  jerseyNumber = 5, 
+  age = 58, 
+  imageUrl = "https://media.gettyimages.com/id/78979414/photo/8th-november-1998-italian-league-serie-a-milan-ac-milan-v-inter-milan-alessandro-costacurta-ac.jpg?s=612x612&w=0&k=20&c=ztnT431IxQvR1XgAXkZEDSx0tidGkxQ7jq57XPvarew=", 
+  teamIcon = "https://icons.iconarchive.com/icons/giannis-zographos/italian-football-club/256/AC-Milan-icon.png", 
+  nationalityIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/20px-Flag_of_Italy.svg.png"}) => {
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
       <Card.Img variant="top" src={imageUrl} />
